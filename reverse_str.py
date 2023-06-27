fe938d1cd1fc1730357d1cb080abf8170e1b167e
Saikumar Konakanchi <120327792+SaikumarKonakanchi0@users.noreply.github.com>
2023-06-28 00:06:41 +0530

    Update reverse_str.py

--- reverse_str.py
+++ reverse_str.py
@@ -1,2 +1,7 @@
-n=input()
-print(n[::-1])
+string = input("Enter a string: ")
+reversed_string = ""
+
+for i in range(len(string) - 1, -1, -1):
+    reversed_string += string[i]
+
+print("Reversed string:", reversed_string)
